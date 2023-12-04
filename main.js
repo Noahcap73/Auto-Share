5138ba8dce6b25d1fdaf88c3f7888baa9dc450ad
Noah Capil <noahcapil@gmail.com>
2023-12-04 10:40:35 -0800

    Creating Home Page

--- main.js
+++ main.js
@@ -1,60 +1,13 @@
-/* Sign in link */
-const signInChange = document.getElementById("sign-in-link");
+const menuIcon = document.getElementById("menu-icon");
+const dropDownMenu = document.getElementById("drop-nav");
 
-/* Sign up link */
-const signUpChange = document.getElementById("sign-up-link");
-
-/* Enter Name Input Field */
-const enterName = document.getElementById("enter-name");
-
-/* Create Password Input Field */
-const createPassword = document.getElementsByName("psw");
-
-/* Confirm Password Input Field */
-const confirmPassword = document.getElementById("confirm-pswd");
-const confirmPasswordInput = document.getElementsByName("cpsw");
-
-/* Button for Sign Up */
-const signUpButton = document.getElementById("signup-btn");
-
-/* Sign In link */
-const signInLink = document.getElementById("sign-in");
-
-/* Sign Up link */
-const signUpLink = document.getElementById("sign-up");
-const signUpText = document.getElementById("sign-up-text");
-
-/* Icons */
-const userIcon = document.getElementById("user-icon");
-
-const username = document.getElementsByName("name");
-
-const changeToSignIn = () => {
-  enterName.classList.add("hidden");
-  createPassword[0].placeholder = "Enter Password";
-  confirmPassword.classList.add("hidden");
-  signUpButton.innerHTML = "Sign in";
-  signUpLink.classList.remove("hidden");
-  signUpLink.classList.add("block");
-  signInLink.classList.add("hidden");
-  signUpText.innerHTML = "Sign in";
+const toggleDropDownMenu = () => {
+  dropDownMenu.classList.toggle("-translate-y-80");
+  menuIcon.classList.toggle("fa-bars");
+  menuIcon.classList.toggle("fa-times");
 };
 
-const changeToSignUp = () => {
-  enterName.classList.remove("hidden");
-  createPassword[0].placeholder = "Create Password";
-  confirmPassword.classList.remove("hidden");
-  signUpButton.innerHTML = "Sign up";
-  signUpLink.classList.add("hidden");
-  signUpLink.classList.remove("block");
-  signInLink.classList.remove("hidden");
-  signUpText.innerHTML = "Sign up";
-};
-
-signInChange.addEventListener("click", function () {
-  changeToSignIn();
-});
-
-signUpChange.addEventListener("click", function () {
-  changeToSignUp();
+menuIcon.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropDownMenu();
 });
