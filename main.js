7ce42925b619af31d83444b03c70ed7bc43bbb86
Noah Capil <noahcapil@gmail.com>
2023-12-04 21:04:09 -0800

    Adding content to index

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const menuIcon = document.getElementById("menu-icon");
 const dropDownMenu = document.getElementById("drop-nav");
+const bannerSection = document.getElementById("banner");
 
 const toggleDropDownMenu = () => {
   dropDownMenu.classList.toggle("-translate-y-80");
   menuIcon.classList.toggle("fa-bars");
   menuIcon.classList.toggle("fa-times");
+  bannerSection.classList.toggle("-translate-y-32");
 };
 
 menuIcon.addEventListener("click", function (e) {
